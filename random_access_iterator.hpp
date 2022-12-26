6065f9230f325314da36d4406495dd9a5fecb2e9
hakkache06 <hakkacheyassin@gmail.com>
2022-12-26 22:08:00 +0100

    Fin Vector-stack

--- random_access_iterator.hpp
+++ random_access_iterator.hpp
@@ -29,13 +29,14 @@ namespace ft {
         this -> _pointer = other._pointer;
         return * this;
       }
-
+        // Prefix increment operator (++it). 
       random_access_iterator & operator++() {
         ++_pointer;
         return * this;
       }
+        // Postfix increment operator (it++). that increments an iterator, but returns the original value of the iterator before it was incremented.
       random_access_iterator operator++(int) {
-        random_access_iterator tmp(_pointer);
+        random_access_iterator tmp =_pointer;
         _pointer++;
         return tmp;
       }
@@ -45,14 +46,16 @@ namespace ft {
       }
 
       random_access_iterator operator--(int) {
-        random_access_iterator tmp(_pointer);
+        random_access_iterator tmp = _pointer;
         _pointer--;
         return tmp;
       }
+        // Addition assignment operator (it += n).
       random_access_iterator & operator += (difference_type n) {
         _pointer += n;
         return * this;
       }
+        // Subtraction assignment operator (it -= n).
       random_access_iterator & operator -= (difference_type n) {
         _pointer -= n;
         return * this;
@@ -62,21 +65,23 @@ namespace ft {
         temp._pointer += n;
         return temp;
       }
-      // 1 5 3 2 1 0
       random_access_iterator operator - (difference_type n) {
-        random_access_iterator temp(_pointer);
+        random_access_iterator temp = _pointer;
         temp._pointer -= n;
         return temp;
       }
       difference_type operator - (const random_access_iterator & other) {
-        return _pointer - other.get_pointer();
+        return _pointer - other.get_p();
       }
+        // Element access operator (it[n]).
       reference operator[](difference_type n) const {
         return * (_pointer + n);
       }
+        // Dereference operator (*it).
       reference operator * () const {
         return * _pointer;
       }
+        // Pointer operator (it.operator->()).
       pointer operator -> () const {
         return & (operator * ());
       }
@@ -86,7 +91,7 @@ namespace ft {
           const value_type > (_pointer);
       }
 
-      pointer get_pointer() const {
+      pointer get_p() const {
         return this -> _pointer;
       }
 
@@ -97,47 +102,47 @@ namespace ft {
   template < class T >
     bool operator == (const random_access_iterator < T > & it1,
       const random_access_iterator < T > & it2) {
-      return (it1.get_pointer() == it2.get_pointer());
+      return (it1.get_p() == it2.get_p());
     };
   template < class T >
     bool operator != (const random_access_iterator < T > & it1,
       const random_access_iterator < T > & it2) {
-      return (it1.get_pointer() != it2.get_pointer());
+      return (it1.get_p() != it2.get_p());
     };
   template < class T >
     bool operator < (const random_access_iterator < T > & it1,
       const random_access_iterator < T > & it2) {
-      return (it1.get_pointer() < it2.get_pointer());
+      return (it1.get_p() < it2.get_p());
     };
   template < class T >
     bool operator > (const random_access_iterator < T > & it1,
       const random_access_iterator < T > & it2) {
-      return (it1.get_pointer() > it2.get_pointer());
+      return (it1.get_p() > it2.get_p());
     };
   template < class T >
     bool operator <= (const random_access_iterator < T > & it1,
       const random_access_iterator < T > & it2) {
-      return (it1.get_pointer() <= it2.get_pointer());
+      return (it1.get_p() <= it2.get_p());
     };
   template < class T >
     bool operator >= (const random_access_iterator < T > & it1,
       const random_access_iterator < T > & it2) {
-      return (it1.get_pointer() >= it2.get_pointer());
+      return (it1.get_p() >= it2.get_p());
     };
   template < class T >
     typename random_access_iterator < T > ::difference_type operator - (const random_access_iterator < T > & it1,
       const random_access_iterator < T > & it2) {
-      return (it1.get_pointer() - it2.get_pointer());
+      return (it1.get_p() - it2.get_p());
     };
   template < class T >
     typename random_access_iterator < T > ::difference_type operator + (const random_access_iterator < T > & it1,
       const random_access_iterator < T > & it2) {
-      return (it1.get_pointer() + it2.get_pointer());
+      return (it1.get_p() + it2.get_p());
     };
   template < class T >
     random_access_iterator < T > operator + (typename random_access_iterator < T > ::difference_type n,
       const random_access_iterator < T > & it) {
-      return (it.get_pointer() + n);
+      return (it.get_p() + n);
     };
 
 }
--- test.cpp
+++ test.cpp
@@ -1,5 +1,18 @@
+#include <iostream>
 
 
 
 
+
+int main()
+{
+
+    int a[5] = {0,1,2,3,4};
+
+      for(int *it = a ;  it!= a+5 ; ++it)
+            std::cout << *it << std::endl;  
+
+
+
+}
 
