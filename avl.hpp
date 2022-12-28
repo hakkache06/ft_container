eaf609eca919ff1f0d15ef300f792fd5714595bf
hakkache06 <hakkacheyassin@gmail.com>
2022-12-28 15:33:57 +0100

    start Avl

--- avl.hpp
+++ avl.hpp
@@ -1,9 +1,30 @@
 
 #pragma once
 
+
+# include <algorithm>
+# include <functional>
+# include <iostream>
+# include "pair.hpp"
+
 namespace ft
-
 {
+  
+  	template <
+		class Key, /*Type de données de clé à stocker dans le map.*/
+        class T, /*Type de données d'élément à stocker dans le map.*/
+        class Compare = std::less<Key>, /*   */
+        class Alloc = std::allocator<ft::pair<const Key,T> > /*   */
+        >
+    class Avl
+    {
+        // Member types
+        
 
+
+
+
+
+    };
 
 }
