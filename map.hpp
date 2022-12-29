0e558fd79bfcfaf2784c11bdb01a8796d1bf3b98
hakkache06 <hakkacheyassin@gmail.com>
2022-12-29 20:27:49 +0100

    DeleteNode pro

--- map.hpp
+++ map.hpp
@@ -174,7 +174,7 @@ namespace ft
                     return(heghit_node(noda->left) - heghit_node(noda->right));
             }
 
-
+      
             node *min_node(node *temp)
             {
                 if (temp)
@@ -259,25 +259,78 @@ namespace ft
                 ///
             }
 
-            node    *delete_node(node *root, const key_type &key)
+
+            node * balanced_node(node *root)
             {
-                    if(root == NULL)
-                        return root;
-                    if (key < root->val.fist)
-                        root->left = delete_node(root->left,key);
-                    else if (key > root.val.fisrt)
-                        root->right = delete_node(root->right,key);
-                    else
+                      // balanced node
+                if (root == NULL)
+                    return root;
+                root->height = 1 + max(height(root->left), height(root->right));
+                int balance = getBalance(root);
+
+                if (balance > 1 && getBalance(root->left) >= 0)
+                    return LRI(root);
+
+                if (balance > 1 && getBalance(root->left) < 0)
+                {
+                    root->left = leftRotate(root->left);
+                    return LRI(root);
+                }
+                if (balance < -1 &&
+                    getBalance(root->right) <= 0)
+                    return LR(root);
+                if (balance < -1 &&
+                    getBalance(root->right) > 0)
+                {
+                    root->right = rightRotate(root->right);
+                    return LR(root);
+                }
+                return root;
+            } 
+
+        node *deleteNode(node *root, const key_type &key)
+        {
+        
+        // Position node 
+            if (root == NULL)
+                return root;
+
+            if (key < root->val.first)) 
+                root->left = deleteNode(root->left, key);
+
+            else if (root->val.first > key))
+                root->right = deleteNode(root->right, key);
+        ///
+            else
+            {
+            // if have child    
+                if ((root->left == NULL) ||(root->right == NULL))
+                {
+                    node *temp = root->left ? root->left : root->right;
+
+                    if (temp == NULL)
                     {
-                        if((root->left == NULL )|| (root->right == NULL))
-                        {
-
-                        }
+                        temp = root;
+                        root = NULL;
                     }
-
-                    
-
+                    else // have child one
+                    {
+                        temp->parent = root->parent;
+                        *root = *temp;
+                    } 
+                    _alloc_node.destroy(temp);
+                    _alloc_node.deallocate(temp, 1);
+                }
+                else // node to be deleted have 2 child
+                {
+                    node *temp = min_node(root->right);
+                    root->val = temp->val;
+                    root->right = deleteNode(root->right,temp->val.first);
+                }
             }
+
+            return balanced_node(root);
+        }
 
 		private:
             node    *_root;
