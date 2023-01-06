da48f2e4dc3d9ef50868c2a0ccd1c12ce6177a9d
Hakkache Yassine <35065372+hakkache06@users.noreply.github.com>
2023-01-06 03:38:03 +0100

    Update nodda.hpp

--- nodda.hpp
+++ nodda.hpp
@@ -2,7 +2,13 @@
 #pragma once 
 
 #include<iostream>
-template <typename T , class alloc>
+template <typename T , class alloc, class compare>
+
+//typedef value_type										value_type;
+//typedef key_type										key_type;
+//typedef mapped_type										mapped_type;
+//Compare													_compare;
+
 class  nodaaa 
         {
             public: 
@@ -50,6 +56,35 @@ class  nodaaa
             // {
             //     return (height);
             // }
+                
+            //return the key of the current node
+	    // key_type get_key() const
+	    // {
+	     // return (this->value->first);
+	    // }
+               //return return a reference to the value of the current node				
+                // mapped_type &get_value()
+                // {
+	        // 	return (this->value->second);
+                // }
+                //return true if they are the same key, otherwise false.
+		//bool is_equal(key_type k)
+		//{
+		//return (this->_compare(this->get_key(), k) == this->_compare(k, this->get_key()));
+		//
+        
+      				/*
+				// compare 2 keys and return if they met the condition bellow
+				// @param k element key
+				// @return true if they met the condition, otherwise false.
+				
+				bool is_lower_bound(key_type k)
+				{
+					return ((this->_compare(this->get_key(), k)) == false); uper bound true
+				}
+				*/
+        
+        
 
         };
 
