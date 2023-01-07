c57df0f650c2a31a01782c385565af64f7d7cfd6
MacBook Pro <azavip@MacBook-Pro-de-yhakkach.local>
2023-01-07 04:49:21 +0100

    update map

--- map.hpp
+++ map.hpp
@@ -42,6 +42,8 @@ namespace ft
         //// iterator 
         typedef ft::tree_iterator<Key,T,const value_type, Alloc, AVL<Key,T,value_type,Alloc> > const_iterator;
         typedef ft::tree_iterator<Key,T,value_type, Alloc,AVL<Key,T,value_type,Alloc> > iterator;
+        typedef typename ft::reverse_iterator < iterator > reverse_iterator;
+        typedef typename ft::reverse_iterator < const_iterator > const_reverse_iterator;
 
         // fin iterator
 
@@ -50,7 +52,7 @@ namespace ft
     public:
      
       Alloc _alloc;
-      Compare _key_compare;
+      Compare _key_comp;
       size_type _size;
       
     // size_t _capacity;
@@ -76,7 +78,7 @@ namespace ft
         {
             
         }
-
+        //insert first -- last
         // ~map()
         // {
         //     avl_tree.dellocate_node(_node); 
@@ -93,90 +95,203 @@ namespace ft
             this->avl_tree.affiche(avl_tree._head);
         }
 
-        iterator  begin()
-        {
-           return iterator(avl_tree.min_noode(avl_tree._head),avl_tree);
-        }
-        iterator  end()
-        {
-           return iterator(nullptr,avl_tree);
-        }
+        iterator begin()
+		{
+ 			return iterator(avl_tree.min_noode(avl_tree._head),avl_tree);
+ 		}
+        const_iterator begin() const
+		{
+ 			return const_iterator(avl_tree.min_noode(avl_tree._head),avl_tree);
+ 		}
 
-        // const_iterator  begin()
-        // {
-        //    return const_iterator(avl_tree.min_noode(avl_tree._head),avl_tree);
-        // }
+		iterator end()
+		{
+			return (iterator(nullptr,avl_tree));
+		}
+		// const_iterator end() const
+		// {
+		// 	return (const_iterator(nullptr,avl_tree));
+		// }
 
-        // const_iterator  end()
-        // {
-        //    return const_iterator(nullptr,avl_tree);
-        // }
+		reverse_iterator rbegin()
+		{
+			return (reverse_iterator(this->end()));
+		}
 
+		// const_reverse_iterator rbegin() const
+		// {
+		// 	return (const_reverse_iterator(this->end()));
+		// }
+		reverse_iterator rend()
+		{
+			return (reverse_iterator(this->begin()));
+		}
 
-        bool empty() const 
-        {
-            if (avl_tree.treesize()==0)
-                return  (true);
-            else    
-                return (false);       
-        }
-        size_t size() const
-        {
-            return (avl_tree.tree_size());
-        }
-
-        size_t  max_size() const
-        {
-            return (avl_tree.alloc_noode.max_size());
-        }
+		// const_reverse_iterator rend() const
+		// {
+		// 	return (const_reverse_iterator(this->begin()));
+		// }
+		bool empty() const
+		{
+			return (this->size == 0);
+		}
+		size_type size() const
+		{
+			return (this->size);
+		}
+		size_type max_size() const
+		{
+			return (this->alloc_node.max_size());
+		}
+		Mapped_type& operator[] (const key_type& k)
+		{
+			return ((*((this->insert(ft::make_pair(k, mapped_type()))).first)).second);
+		}
         pair<iterator,bool> insert (const value_type& val)
-        {   
-                iterator it = end();
-                nodaaa<Key,T,value_type,Alloc> * temp = avl_tree.find_key(avl_tree._head,val.first);
-               if(temp == nullptr)
-                {   
-                    avl_tree.insert(avl_tree._head,val);  
-                    std::cout << "ana henya" << std::endl;
-                    return (pair<iterator,bool>(iterator(temp,avl_tree),true));
-                } 
-            return (pair<iterator,bool>(it,false));
-        }    
+		{
+			nodaaa<Key,T,value_type>	*tmp;
 
+			tmp = this->avl_tree.find_key(avl_tree._head,x.first););
+			this->avl_tree.insert(avl_tree._head.val);
+			if (tmp)
+				return (pair<iterator, bool>(tmp, false));
+			++this->size;
+			return (pair<iterator, bool>(this->avl_tree.fin_node(val.first), true));
+		}
+        iterator insert (iterator position, const value_type& val)
+ 		{
+ 			(void) position;
+ 			return (this->insert(avl_tree._head,val.first);
+ 		}
+        template <class InputIterator>
+		void insert (InputIterator first, InputIterator last)
+		{
+			while (first != last)
+				this->insert(avl_tree._head,*(first++));
+		}
+        void erase (iterator position)
+		{
+			if (this->avl_tree.find_key(avl_tree._head,(*position).first))
+			{
+				this->avl_tree.delete_node(avl_tree._head,(*position).first);
+				--this->size;
+			}
+		}
+        size_type erase (const key_type& k)
+		{
+			if (this->avl_tree.find_key(avl_tree._head,k)
+			{
+				this->avl_tree.delete_node(avl_tree._head,k);
+				--this->size;
+				return (1);
+			}
+			return (0);
+		}
+        void erase (iterator first, iterator last)
+		{
+			ft::Vector<key_type> tmp;
+			typename ft::Vector<key_type>::iterator it;
 
-        
-        iterator insert(iterator position ,const value_type &x)
+			while (first != last)
+				tmp.push_back((first++)->first);
+			for (it = tmp.begin(); it != tmp.end(); it++)
+				this->erase(*it);
+		}
+        void swap (Map& x)
+     	{
+	        std::swap(this->_alloc, x._alloc);
+ 	        std::swap(this->_key_comp, x._key_comp);
+ 	        std::swap(this->_size, x._size);
+ 	        this->avl_tree.swap(x.avl_tree);
+ 	    }
+        void clear()
+ 	    {
+ 	        this->erase(this->begin(), avl_tree.max_noode());
+ 	    }
+
+        iterator find (const key_type& k)
         {
-            (void)position;
-            nodaaa <Key,T,value_type,Alloc> * temp =  avl_tree.find_key(avl_tree._head,x.first);
+            nodaaa<Key,T,value_type> *pointer;
 
-            if (temp == NULL )
-            {
-                avl_tree._head = avl_tree.insert(avl_tree._head,x);
-                return (iterator(avl_tree._head,avl_tree));
-            }   
-            return iterator(temp,avl_tree);
+            pointer = this->find_key(avl_tree._head,k);
+            return (pointer ? iterator(pointer) : this->end());
         }
 
-        // void insertp(const value_type &x)
+        // const_iterator find (const key_type& k) const
         // {
-        //     nodaaa <Key,T,value_type,Alloc> * temp =  avl_tree.find_key(avl_tree._head,x.first);
+        //     nodaaa<Key,T,value_type> *pointer;
 
-        //     if (temp == NULL )
-        //         avl_tree._head = avl_tree.insert(avl_tree._head,x);
-
+        //     pointer = this->find_key(avl_tree._head,k);
+        //     return (pointer ? iterator(pointer) : this->end());
         // }
-        ///If k matches the key of an element in the container, the function returns a reference to its mapped value
-        mapped_type& operator[] (const key_type& k)
-        {
-            return (*((this->insert(ft::make_pair(k,mapped_type()))).first)).second;
+
+        size_type count (const key_type& k) const
+     	{
+     	  return (avl_tree.find_key(avl_tree._head,k) != NULL);
         }
-        // template <class InputIterator> 
-        // void insert (InputIterator first, InputIterator last)
+
+        // iterator lower_bound (const key_type& k)
+     	// {
+ 		// 	nodaaa<Key,T,value_type> *tmp;
+
+ 		// 	tmp = this->avl_tree._head.lower_bound(k);
+ 		// 	return (tmp ? iterator(tmp) : this->end());
+ 		// }
+        
+        // iterator upper_bound (const key_type& k)
         // {
-             
-        // };
+        //     node *tmp;
 
+        //     tmp = this->avl_tree._head->is_upper_boud(k);
+        //     return (tmp ? iterator(tmp) : this->end());
+        // }
 
+        //pair<iterator,iterator> equal_range (const key_type& k)
+// // 		{
+// // 			return (ft::make_pair(this->lower_bound(k), this->upper_bound(k)));
+// // 		}
+	
+// // 		allocator_type get_allocator() const
+// // 		{
+// // 			return (this->_alloc);
+// // 		}
+
+// // 	public:
+// // 	    friend bool operator==(const Map &lhs, const Map &rhs)
+// // 		{
+// // 			return (lhs.size() == rhs.size() && ft::equal(lhs.begin(), lhs.end(), rhs.begin()));
+// // 		}
+// //         friend bool operator!=(const Map& lhs, const Map& rhs)
+// // 		{
+// // 			return !(lhs == rhs);
+// // 		}
+
+// //         friend bool operator<(const Map& lhs,const Map& rhs)
+// // 		{
+// // 			return (ft::lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), rhs.end()));
+// // 		}
+
+// //         friend bool operator<=(const Map& lhs,const Map& rhs)
+// // 		{
+// // 			return !(rhs < lhs);
+// // 		}
+
+// //         friend bool operator>(const Map& lhs, const Map& rhs )
+// // 		{
+// // 			return (rhs < lhs);
+// // 		}
+
+// //         friend bool operator>=(const Map& lhs,const Map& rhs )
+// // 		{
+// // 			return !(rhs > lhs);
+// // 		}
+// // };
+
+// // template <class Key, class T, class Compare, class Alloc>
+// // void swap (Map<Key,T,Compare,Alloc>& x, Map<Key,T,Compare,Alloc>& y)
+// // {
+// // 	x.swap(y);
+// // }
 
     private:
     
